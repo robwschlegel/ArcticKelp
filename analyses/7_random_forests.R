2e9b33fd4e9d86417a3d595c519d5efe3718aa93
robwschlegel <robwschlegel@gmail.com>
2019-11-07 21:35:45 -0500

    Additional forest modelling

--- analyses/7_random_forests.R
+++ analyses/7_random_forests.R
@@ -46,7 +46,9 @@ kelp_var <- kelp_all %>%
 # The data.frame that will be fed to the model
 # The Quadrat information is fairly random, as it should be, and so isn't used in the model
 # The substrate and depth values also score consistently in the bottom of importance so aren't used
-data1 <- select(kelp_var, kelp.cover:toce)
+data1 <- select(kelp_var, kelp.cover:toce, -lon, -lat) %>% 
+  mutate(kelp.cover = as.factor(kelp.cover)) #%>% 
+  # mutate(kelp.cover = factor(kelp.cover, levels = as.character(seq(0, 100, by = 10))))
 set.seed(666)
 train <- sample(nrow(data1), 0.7*nrow(data1), replace = FALSE)
 train_set <- data1[train,]
@@ -56,16 +58,16 @@ valid_set <- data1[-train,]
 rf_mtry_test <- function(mtry_num){
   set.seed(666)
   test_rf <- randomForest(kelp.cover ~ ., data = train_set, mtry = mtry_num, ntree = 1000,
-                          importance = TRUE, na.action = na.omit)
+                          importance = TRUE)
   pred_test <- predict(test_rf, train_set, type = "class")
   pred_accuracy <- data.frame(mtry = mtry_num,
-                              acc = round(mean(round(pred_test, -1) == train_set$kelp.cover)*100))
+                              acc = round(mean(pred_test == train_set$kelp.cover)*100))
   return(pred_accuracy)
 }
-lapply(1:9, rf_mtry_test) # It looks like an mtry = 1 is best
+lapply(1:9, rf_mtry_test) # It looks like an mtry >= 2 is best
 
 # Random forest model based on all quadrat data
-kelp_rf <- randomForest(kelp.cover ~ ., data = train_set, mtry = 1, ntree = 1000,
+kelp_rf <- randomForest(kelp.cover ~ ., data = train_set, mtry = 2, ntree = 1000,
                         importance = TRUE, na.action = na.omit)
 summary(kelp_rf)
 round(importance(kelp_rf), 2)
@@ -73,13 +75,13 @@ varImpPlot(kelp_rf)
 
 # Predicting on training set
 pred_train <- predict(kelp_rf, train_set, type = "class") # type = 'response' also works but returns the same results
-table(round(pred_train, -1), train_set$kelp.cover)  
-mean(round(pred_train, -1) == train_set$kelp.cover)*100 # 23% accuracy...
+table(pred_train, train_set$kelp.cover)  
+mean(pred_train == train_set$kelp.cover)*100 # 43% accuracy...
 
 # Predicting on Validation set
 pred_valid <- predict(kelp_rf, valid_set, type = "class")
-table(round(pred_valid, -1), valid_set$kelp.cover)  
-mean(round(pred_valid, -1) == valid_set$kelp.cover)*100 # 4% accuracy... sad face
+table(pred_valid, valid_set$kelp.cover)  
+mean(pred_valid == valid_set$kelp.cover)*100 # 29% accuracy...
 
 
 # forestRK method ---------------------------------------------------------
@@ -89,8 +91,8 @@ mean(round(pred_valid, -1) == valid_set$kelp.cover)*100 # 4% accuracy... sad fac
 # Step 1: Remove all NA's and NaN's from the original dataset
 # The method prefers categorical/factor response variables
 data2 <- na.omit(data1) %>%  # This is the same data.frame a the previous model method
-  mutate(kelp.cover = as.factor(kelp.cover)) %>% 
-  mutate(kelp.cover = factor(kelp.cover, levels = as.character(seq(0, 100, by = 10))))
+  mutate(kelp.cover = as.factor(kelp.cover))# %>% 
+  # mutate(kelp.cover = factor(kelp.cover, levels = as.character(seq(0, 100, by = 10))))
 # levels(data2$kelp.cover)
 
 # Step 2: Seperate the dataset into a chunk that stores covariates of both 
@@ -102,16 +104,11 @@ y <- data2[vec, 1]
 
 # Step 3: Numericize the data frame X by applying the x.organizer function
 # and split X into a training and a test set
-X1 <- x.organizer(X, encoding = "bin") # Binary Encoding applied
-X2 <- x.organizer(X, encoding = "num") # Numeric Encoding applied
+X1 <- x.organizer(X, encoding = "num") # Numeric Encoding applied
 
-## train and test set from the Binary Encoding
+## train and test set from the Numeric ncoding
 x.train1 <- X1[vec,]
 x.test1 <- X1[-vec,]
-
-## train and test set from the Numeric Encoding
-x.train2 <- X2[vec,]
-x.test2 <- X2[-vec,]
 
 # Step 4: Numericize the vector y by applying the y.organizer function
 y.train <- y.organizer(y)$y.new # a vector storing the numericized class type
@@ -128,14 +125,15 @@ tree.gini <- construct.treeRK(x.train1, y.train, min.num.obs.end.node.tree = 2,
 # Construct rkTree based on entropy splitting
   ## default for entropy is TRUE
   ## default for min.num.obs.end.node.tree is 5
-tree.entropy <- construct.treeRK(x.train2, y.train)
+tree.entropy <- construct.treeRK(x.train1, y.train)
 
 ## Visualise model
 
 # plotting the tree.gini rkTree obtained via construct.treeRK function
-draw.treeRK(tree.gini, y.factor.levels, font = "Times", node.colour = "white", 
+draw.treeRK(tree.gini, y.factor.levels[c(-1)], font = "Times", node.colour = "white", 
             text.colour = "dark blue", text.size = 0.6, tree.vertex.size = 30, 
             tree.title = "Decision Tree", title.colour = "dark green")
+# ggsave("graph/forestRK_tree.png")
 
 ## Model predictions
 
@@ -146,17 +144,17 @@ prediction.df <- pred.treeRK(X = x.test1, tree.gini)$prediction.df
 # prediction.df[1:5, dim(prediction.df)[2]]
 
 # display the list of hierarchical flag from the test set with Numeric Encoding
-pred.treeRK(X = x.test2, tree.entropy)$flag.pred
+pred.treeRK(X = x.test1, tree.entropy)$flag.pred
 
 ## More models
 
-# Make a forestRK model based on the training data with Binary Encoding and with Gini Index as the splitting criteria
+# Make a forestRK model based on the training data with Numeric Encoding and with Gini Index as the splitting criteria
 # normally nbags and samp.size would be much larger than 30 and 50
 forestRK.1 <- forestRK(x.train1, y.train, nbags = 30, samp.size = 50, entropy = FALSE)
 
 # Make a forestRK model based on the training data with Numeric Encoding and with Entropy as the splitting criteria
 # normally nbags and samp.size would be much larger than 30 and 50
-forestRK.2 <- forestRK(x.train2, y.train, nbags = 30, samp.size = 50)
+forestRK.2 <- forestRK(x.train1, y.train, nbags = 30, samp.size = 50)
 
 ## Predictions
 
@@ -171,7 +169,7 @@ pred.forest.rk1 <- pred.forestRK(x.test = x.test1, x.training = x.train1,
 # Make predictions on the test set x.test2 based on the forestRK model constructed from
 # x.train2 and y.train (recall: these are the training data with Numeric Encoding)
 # after using Entropy as splitting criteria (entropy == TRUE)
-pred.forest.rk2 <- pred.forestRK(x.test = x.test2, x.training = x.train2, 
+pred.forest.rk2 <- pred.forestRK(x.test = x.test1, x.training = x.train2, 
                                  y.training = y.train, nbags = 100, 
                                  samp.size = 100, y.factor.levels = y.factor.levels, 
                                  entropy = TRUE)
@@ -221,15 +219,10 @@ ggsave("graph/forestRK_variable_importance.png", height = 5, width = 6)
 
 ## Comparing performance
 
-# Overall prediction accuracy (in percentage) when training data was modified with Binary Encoding
-# and the Gini Index was used as splitting criteria.
-y.test <- data2[-vec, 1] # this is an actual class type of test observations
-mean(pred.forest.rk1$pred.for.obs.forest.rk == as.vector(y.test))*100 # 2% accuracy...
-
 # Overall prediction accuracy (in percentage) when training data was modified with numeric Encoding
 # and the Entropy was used as splitting criteria.
 y.test <- data2[-vec, 1] # this is an actual class type of test observations
-mean(pred.forest.rk1$pred.for.obs.forest.rk == as.vector(y.test))*100 # 2% accuracy...
+mean(pred.forest.rk1$pred.for.obs.forest.rk == as.vector(y.test))*100 # 6% accuracy...
 
 
 # A different approach ----------------------------------------------------
