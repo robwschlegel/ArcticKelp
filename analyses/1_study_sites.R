408efa47d0642a426b90a6b76079cfc768bc0593
Robert William Schlegel <robwschlegel@gmail.com>
2020-01-13 16:44:06 -0500

    Added some reading and the CANA data

--- analyses/1_study_sites.R
+++ analyses/1_study_sites.R
@@ -28,25 +28,44 @@ study_sites <- readxl::read_xlsx("metadata/Kelp cover photograph 2019 GPS locati
 # Set bounding box for study
 bbox_arctic <- c(-90, -50, 50, 80)
 
+# Load the CANA data
+  # These data are from the archives of the Canadian Museum of Nature
+  # Each line represents one collection
+  # RWS: I originally manually edited the column 3 name, but then 
+  # everything went pear shaped, so I'm leaving the file untouched
+CANA_kelp <- read.csv("data/CANA_Arctic_Kelp.csv")
+colnames(CANA_kelp)[3] <- "Year"
+
 
 # Visualise ---------------------------------------------------------------
 
- ggplot(data = study_sites, aes(x = lon, y = lat)) +
-   borders(fill = "grey70", colour = "black") +
-   geom_point(colour = "red", size=4) +
+# Unlabelled ArcticKelp points
+ggplot(data = study_sites, aes(x = lon, y = lat)) +
+  borders(fill = "grey70", colour = "black") +
+  geom_point(colour = "red", size = 4) +
   # geom_label_repel(aes(label = site)) +
-   coord_cartesian(xlim = c(bbox_arctic[1], bbox_arctic[2]),
-                   ylim = c(bbox_arctic[3], bbox_arctic[4])) +
-   labs(x = NULL, y = NULL)
- # ggsave("graph/study_area_points.png", width = 9, height = 7)
- 
- ggplot(data = study_sites, aes(x = lon, y = lat)) +
-   borders(fill = "grey70", colour = "black") +
-   geom_point(colour = "red", size=4) +
-   geom_label_repel(aes(label = site)) +
-   coord_cartesian(xlim = c(bbox_arctic[1], bbox_arctic[2]),
-                   ylim = c(bbox_arctic[3], bbox_arctic[4])) +
-   labs(x = NULL, y = NULL)
- # ggsave("graph/study_area_pointswlabels.png", width = 9, height = 7)
+  coord_cartesian(xlim = c(bbox_arctic[1], bbox_arctic[2]),
+                  ylim = c(bbox_arctic[3], bbox_arctic[4])) +
+  labs(x = NULL, y = NULL)
+# ggsave("graph/study_area_points.png", width = 9, height = 7)
 
- 
+# Labelled ArcticKelp points
+ggplot(data = study_sites, aes(x = lon, y = lat)) +
+  borders(fill = "grey70", colour = "black") +
+  geom_point(colour = "red", size = 4) +
+  geom_label_repel(aes(label = site)) +
+  coord_cartesian(xlim = c(bbox_arctic[1], bbox_arctic[2]),
+                  ylim = c(bbox_arctic[3], bbox_arctic[4])) +
+  labs(x = NULL, y = NULL)
+# ggsave("graph/study_area_pointswlabels.png", width = 9, height = 7)
+
+# CANA collection points
+ggplot(data = CANA_kelp, aes(x = Longitude, y = Latitude)) +
+  borders(fill = "grey70", colour = "black") +
+  geom_point(colour = "red", size = 3) +
+  # geom_label_repel(aes(label = site)) +
+  coord_cartesian(xlim = c(-180, 180),
+                  ylim = c(25, 85)) +
+  labs(x = NULL, y = NULL)
+# ggsave("graph/CANA_points.png", width = 16, height = 5)
+
