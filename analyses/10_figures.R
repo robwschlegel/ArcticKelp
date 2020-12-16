83cea27ae06cc0fb9c2348f5499c542da4e74c1e
Robert <robert.schlegel@imev-mer.fr>
2020-12-16 16:49:33 +0100

    Getting some thoughts organised for the manuscript tables and figures

--- analyses/10_figures.R
+++ analyses/10_figures.R
@@ -5,6 +5,46 @@
 # Setup -------------------------------------------------------------------
 
 
+
+# Table 1 -----------------------------------------------------------------
+
+# Importance of variables by kelps and models
+
+
 # Figure 1 ----------------------------------------------------------------
 
 # The map of the study area with the sample points
+# Also add colour points for abundance (%)
+
+
+# Figure 2 ----------------------------------------------------------------
+
+# Relationship with important variables and kelp PA and abundance
+
+
+# Figure 3 ----------------------------------------------------------------
+
+# The ensemble model results
+
+
+# Figure 4 ----------------------------------------------------------------
+
+# The random forest model results
+# Combine into one dataframe to have the same legend via facets
+
+
+# Figure 5 ----------------------------------------------------------------
+
+# Future ensemble model projections
+
+
+# Figure 6 ----------------------------------------------------------------
+
+# Future random forest model projections
+
+
+# Figure 7 ----------------------------------------------------------------
+
+# Comparison of the present modelling approaches
+
+
